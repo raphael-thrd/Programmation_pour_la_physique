62ce4a66df8ee5829babbc8307a50a5fd3ee2c3e
raphael-thrd <raphael.thouroude@laposte.net>
2025-05-18 11:11:24 +0200

    Add files via upload

---
+++ 2023session1/exo2.py
@@ -0,0 +1,86 @@
+#====on défini la fonction J0============
+import scipy.special
+
+def J0(x,N=20):
+	J=0 #variable dynamique
+	for k in range(N+1):
+		J+=(-1)**k/scipy.special.factorial(k)**2*(x/2)**(2*k)
+	return J
+
+#=====on trace le graphique de notre fonction==========
+
+import matplotlib.pyplot as plt 
+import numpy as np
+
+N=11
+
+X=[i for i in range(N)] #fait une liste avec les elements de la boucle
+Y=np.zeros(N)
+Y_scipy=np.zeros(N)
+
+for i in range(N):
+	Y[i]=J0(i)
+	Y_scipy[i]=scipy.special.j0(i)
+
+plt.plot(X,Y,label="J0")
+plt.plot(X,Y_scipy,label="J0 scipy")
+
+plt.legend()
+plt.show()
+
+
+#======recherche des Zeros============================================
+
+#on utilise la methode de Newton
+
+#fonction de derivation
+def d(f,epsilon=10e-5):
+    
+	def g(x):
+		return (f(x+epsilon)-f(x-epsilon))/(2*epsilon)
+	return g
+	
+dJ0=d(J0)
+
+#....Newton.....
+
+def Newton(f,fprime, x0, epsilon=1e-3,maxit=200):
+	i=0 #sert pour maxit
+	x=x0
+	x_ancien=x0+2*epsilon
+	
+	while abs(x-x_ancien)>epsilon:
+		if fprime(x)==0:
+			print("division par zéro, arret du programme")
+			return
+		x_ancien=x
+		x=x-f(x)/fprime(x)
+		
+		if i>maxit:
+			print("pas de convergence")
+			return
+		print(f"Zeros trouve à x={x:.3f}")
+		return x
+		
+
+#..maintenant on cherche les zeros par itération....
+
+for n in [3,5,8]: #on a vu sur notre graphique que les premiers zeros sont vers 2, 5, 8
+	zero=Newton(J0,dJ0,n)
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2023session1/exo3.py
@@ -0,0 +1,62 @@
+import numpy as np
+import scipy as sc
+
+#....on definit des fonctions auxilliaires....
+
+def norme(v): #norme d'un vecteur
+	return np.sqrt(np.sum(v**2))
+
+def projeter(v,w, epsilon=1e-10):
+	vnorme=norme(v)
+	if vnorme<epsilon: 
+		return np.zeros(v.shape[0]) #projection sur un vecteur = vecteur nul
+		
+	resultat= np.copy(v)
+	resultat *= v@w
+	resultat /=vnorme**2
+	return resultat
+
+
+#....on va definir les algorithmes de gram schimdt et QR....
+
+def gram_schmidt(A,epsilon=1e-10):
+	n=A.shape[0]
+	resultat=np.copy(A)
+	for i in range(n):
+		v=resultat[:,i] #pour tout vecteur de colone v
+		vnorme=norme(v)
+		if vnorme>=epsilon: #normaliser (sauf si nul)
+			v/=vnorme
+		for j in range(i+1,n): #pour toute colone apres v, 
+			resultat[:,j]-= projeter(v,resultat[:,j],epsilon)#soustraire sa projection sur v
+	return resultat
+			
+def decomp_QR(A, epsilon=1e-10):
+	Q=gram_schmidt(A,epsilon)
+	R=Q.T@A
+	return Q,R
+	
+#======On resout maintenant l'equation=====================
+#On sait que Rx=Qt*b, donc x=Inverse(R)*Qt*b
+
+A=np.array([[1.5,4,2],
+	[3,-2,0],
+	[1,1,5]])
+b=np.array([[7],[5],[-0.5]])
+
+Q,R=decomp_QR(A)
+
+x=np.linalg.inv(A)@Q.T@b
+
+print(x)
+
+
+
+
+
+
+			
+			
+			
+			
+			
